9d75328630e7621c4f78e1f8560a2ea0ec5bc5cc
lizzkaa <159809456+lizzkaa@users.noreply.github.com>
2024-02-17 05:21:34 +0300

    Add files via upload

---
+++ laba2_2/lb2.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+class tetrahedron {
+    int a;
+
+public:
+    tetrahedron(int valueA)
+    {
+        setA(valueA);
+    }
+
+    void setA(int valueA) {
+        a = valueA;
+    }
+
+    int getA() {
+        return a;
+    }
+
+    float square_t() {
+        return sqrt(3) * pow(a, 2);
+    }
+    float volume_t() {
+        return (pow(a, 3) * sqrt(2)) / 12;
+    }
+    void print() {
+        std::cout << "сторона:" << a << ", Площадь поверхности:" << square_t() << ", Объем" << volume_t();
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    int x;
+    std::cout << "Введите сторону тетраэдра:";
+    std::cin >> x;
+    tetrahedron a(x);
+    a.square_t();
+    a.print();
+    system("pause");
+}
